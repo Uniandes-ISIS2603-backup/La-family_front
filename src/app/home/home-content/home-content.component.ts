0241bcf5f73d0e8ef04c5b25fa418a535a437f6c
Sebastian Santos Bautista <s.santosb@uniandes.edu.co>
2019-12-12 16:17:05 -0500

    ASDD

--- src/app/home/home-content/home-content.component.ts
+++ src/app/home/home-content/home-content.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit, Input} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {BookService} from '../../book/book.service';
+import {Book} from '../../book/book';
 import 'rxjs/add/operator/filter';
 
 @Component({
@@ -12,17 +14,28 @@ export class HomeContentComponent implements OnInit {
     /**
     * The component's constructor
     */
-    constructor(private route: ActivatedRoute) {}
+    constructor(private bookService: BookService, private route: ActivatedRoute) {}
 
+    books:Book[];
+    allbooks: string = 'no';
     /**
     * This method retrieves all the books in the Bookstore to show them in the list
     */
+    getBooks(): void {
+        this.bookService.getBooks()
+            .subscribe(books => {
+                this.books = books;
+                console.log("El tamaño es de: " + this.books.length)
+            });
+    }
 
     /**
     * The method which initializes the component
     */
     ngOnInit() {
-        
+            this.getBooks();
+        }
     }
+
 
 }
